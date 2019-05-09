f915313cdba5660cff96f9714b76f0db4bea300e
Stevan Earl <stevan.earl@asu.edu>
2019-05-09 12:20:15 -0700

    added site details to database build; ~Spp to text

--- create-database/UC-build-database.R
+++ create-database/UC-build-database.R
@@ -41,11 +41,47 @@ WITH (
 );
 ')
 
-# insert testing data
-# dbExecute(pg,"
-# INSERT INTO urban_chiroptera.sites (site_id) VALUES ('test site');
-# INSERT INTO urban_chiroptera.sites (site_id) VALUES ('w15');          
-# ")
+sites <- read_csv('create-database/SiteInfo.csv') %>% 
+  filter(!is.na(`Site Name`)) %>% 
+  select(-`Site #`) %>% 
+  mutate(
+    Latitude = as.numeric(Latitude),
+    Longitude = as.numeric(Longitude)
+  )
+
+if (dbExistsTable(pg, c('urban_chiroptera', 'sites_temp')))
+  dbRemoveTable(pg, c('urban_chiroptera', 'sites_temp'))
+dbWriteTable(pg,
+             c('urban_chiroptera', 'sites_temp'),
+             value = sites,
+             row.names = F
+)
+
+dbExecute(pg, '
+INSERT INTO urban_chiroptera.sites
+(
+  site_id,
+  cap_site,
+  latitude,
+  longitude,
+  urban_value
+)
+(
+SELECT
+  "Site Name",
+  "CAP Site",
+  "Latitude",
+  "Longitude",
+  "Urban Value"
+FROM
+urban_chiroptera.sites_temp
+);'
+)
+  
+# clean up taxa_temp
+if (dbExistsTable(pg, c('urban_chiroptera', 'sites_temp')))
+  dbRemoveTable(pg, c('urban_chiroptera', 'sites_temp')
+  )
 
 
 # taxa --------------------------------------------------------------------
@@ -150,11 +186,12 @@ CREATE TABLE urban_chiroptera.surveys
   monitoring_night DATE,
   num_pulses_autoid INTEGER,
   num_pulses_detected INTEGER,
+  most_likely_spp TEXT,
   pulse_freq_mean NUMERIC,
   pulse_freq_stdev NUMERIC,
   pulse_dur_mean NUMERIC,
   pulse_dur_stdev NUMERIC,
-  calls_sec FLOAT, --support Infinity
+  calls_sec FLOAT, --FLOAT to support Infinity
   mean_hi_freq NUMERIC,
   mean_low_freq NUMERIC,
   mean_up_slope NUMERIC,
@@ -177,10 +214,11 @@ WITH (
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.high_freq_bat IS 'sonobat output: HiF: high frequency bat';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.low_freq_bat IS 'sonobat output: LoF: low frequency bat';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.call_quality IS 'sonobat output: Call Quality: quality of call (0=poor, 1=good, 2=voucher call/best quality';")
-dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.manual_id IS 'sonobat output: Species Manual ID: manual species ID';")
-dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.auto_id IS 'sonobat output: SppAccp: automatic species ID';")
+# dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.manual_id IS 'sonobat output: Species Manual ID: manual species ID';")
+# dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.auto_id IS 'sonobat output: SppAccp: automatic species ID';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.num_pulses_autoid IS 'sonobat output: #Maj: number of call pulses auto identified as species';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.num_pulses_detected IS 'sonobat output: #Accp: number of call pulses detected';")
+dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.most_likely_spp IS 'sonobat output: ~Spp: most likely species';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.pulse_freq_mean IS 'sonobat output: Fc mean: mean call pulse frequency (kHz)';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.pulse_freq_stdev IS 'sonobat output: Fc StdDev: standard dev call frequency	(kHz)';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.pulse_dur_mean IS 'sonobat output: Dur mean: mean call pulse duration (ms)';")
@@ -196,30 +234,6 @@ dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.file_length IS 'sonobat
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.version IS 'sonobat output: Version: METADATA REQUIRED';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.autoid_quality IS 'sonobat output: AccpQuality: quality metric for auto identification';")
 dbExecute(pg,"COMMENT ON COLUMN urban_chiroptera.surveys.max_pulses IS 'sonobat output: Max#CallsConsidered: max number of call pulses considered';")
-
-
-# bat_observations --------------------------------------------------------
-
-if (dbExistsTable(pg, c('urban_chiroptera', 'bat_observations')))
-  dbRemoveTable(pg, c('urban_chiroptera', 'bat_observations')
-  )
-
-dbExecute(pg,'
-CREATE TABLE urban_chiroptera.bat_observations
-(
-  id SERIAL PRIMARY KEY,
-  survey_id INTEGER NOT NULL,
-  sonobat_id_type INTEGER NOT NULL,
-  taxa_id INTEGER NOT NULL,
-  FOREIGN KEY (survey_id) REFERENCES urban_chiroptera.surveys (id),
-  FOREIGN KEY (sonobat_id_type) REFERENCES urban_chiroptera.sonobat_id_types (id),
-  FOREIGN KEY (taxa_id) REFERENCES urban_chiroptera.taxa (id),
-  UNIQUE(survey_id, sonobat_id_type)
-)
-WITH (
-  OIDS=FALSE
-);
-')
 
 
 # sonobat_id_types --------------------------------------------------------
@@ -245,9 +259,33 @@ INSERT INTO urban_chiroptera.sonobat_id_types(
 ) VALUES 
 ('Species Manual ID', 'manual species ID'),
 ('SppAccp', 'automatic species ID'),
-('~Spp', 'most likely species'),
+--('~Spp', 'most likely species'),
 ('1st', '1st choice species'),
 ('2nd', '2nd choice species'),
 ('3rd', '3rd choice species'),                
 ('4th', '4th choice species')
 ;")
+
+
+# bat_observations --------------------------------------------------------
+
+if (dbExistsTable(pg, c('urban_chiroptera', 'bat_observations')))
+  dbRemoveTable(pg, c('urban_chiroptera', 'bat_observations')
+  )
+
+dbExecute(pg,'
+CREATE TABLE urban_chiroptera.bat_observations
+(
+  id SERIAL PRIMARY KEY,
+  survey_id INTEGER NOT NULL,
+  sonobat_id_type INTEGER NOT NULL,
+  taxa_id INTEGER NOT NULL,
+  FOREIGN KEY (survey_id) REFERENCES urban_chiroptera.surveys (id),
+  FOREIGN KEY (sonobat_id_type) REFERENCES urban_chiroptera.sonobat_id_types (id),
+  FOREIGN KEY (taxa_id) REFERENCES urban_chiroptera.taxa (id),
+  UNIQUE(survey_id, sonobat_id_type)
+)
+WITH (
+  OIDS=FALSE
+);
+')
