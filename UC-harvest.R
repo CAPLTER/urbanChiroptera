f1ee3511a282bd0443ea3eaa8186be2b026d25f2
Jessie Dwyer <jdwyer4@asu.edu>
2021-11-29 15:12:53 -0700

    updated file
    updated file to only grab manual species identifications (sonobat_id_types = 1)

--- UC-harvest.R
+++ UC-harvest.R
@@ -7,6 +7,9 @@
 
 library(tidyverse)
 library(RPostgreSQL)
+library(tsibble)
+library(dplyr)
+library(tidyr)
 
 
 # database connection -----------------------------------------------------
@@ -48,27 +51,26 @@ SELECT
 FROM urban_chiroptera.surveys
 RIGHT JOIN urban_chiroptera.bat_observations BO ON (surveys.id = BO.survey_id)
 JOIN urban_chiroptera.taxa ON (BO.taxa_id = taxa.id)
-JOIN urban_chiroptera.sonobat_id_types SIT ON (SIT.id = BO.sonobat_id_type)
 JOIN urban_chiroptera.sites ON (sites.id = surveys.site_id)
-JOIN urban_chiroptera.surveyors ON (surveyors.id = surveys.surveyor_id);
+JOIN urban_chiroptera.surveyors ON (surveyors.id = surveys.surveyor_id)
+JOIN urban_chiroptera.sonobat_id_types SIT ON (SIT.id = BO.sonobat_id_type)
+WHERE
+    BO.sonobat_id_type = 1;
 ")
 
 currentData %>%
   spread(key = observation_type_description, value = species_code) %>% View()
 str(currentData)
+print(currentData)
+write.csv(mutate(currentData, time=format(call_timestamp, "%Y-%m-%d %H:%M:%S")), file="currentData.csv", row.names=FALSE)
 
-#subset of voucher files
-data_subset <- currentData %>%
-  filter(call_quality == 2) %>%
-  select(site_id, monitoring_night, species_code, call_quality)
-print(data_subset)
+#subsets--------------------------------------------------------------------------------------------------------------------
 
-#subset of voucher files of one species 
+#subset of voucher files of one species
 PAHE_subset <- currentData %>%
-  filter(call_quality == 2, species_code == 'PAHE') %>%
-  select(site_id, monitoring_night, species_code, call_quality)
+  filter(species_code == 'PAHE') %>%
+  select(site_id, monitoring_night, call_timestamp, species_code)
 print(PAHE_subset)
-view(PAHE_subset)
 
 #subset of voucher files at one site
 site_subset <- currentData %>%
@@ -81,6 +83,66 @@ night_subset <- currentData %>%
   filter(call_quality == 2, monitoring_night == '2019-01-14') %>%
   select(site_id, monitoring_night, species_code, call_quality)
 print(night_subset)
+
+#subset of voucher files of one species one site
+PAHE_subset <- currentData %>%
+  filter(species_code == 'PAHE', site_id == '5_10') %>%
+  select(site_id, monitoring_night, call_timestamp, species_code)
+print(PAHE_subset)
+#class(NYSP_subset)
+write.csv(PAHE_subset, file = '~/Desktop/PAHE_subset_50.csv')
+#write.csv(mutate(NYSP_subset, time=format(call_timestamp, "%Y-%m-%d %H:%M:%S")), file="NYSP.csv", row.names=FALSE)
+
+# read data from file, ensure UTC-7
+Data <- read_csv("~/Desktop/currentData.csv",
+                 locale = locale(tz = "America/Phoenix"))
+
+# filter by time
+Data <- as_tsibble(x = Data,
+                  key = NULL,
+                  index = "call_timestamp",
+                  regular = FALSE) %>%
+  fill_gaps(.full = TRUE) %>%
+  mutate(diff = difference(call_timestamp)) %>%
+  filter(diff > 15 | is.na(diff))
+
+
+data_subset <- currentData %>%
+  mutate(diff = difference(call_timestamp)) %>%
+  filter(diff > 15 | is.na(diff))
+  select(site_id, monitoring_night, species_code, call_timestamp)
+print(data_subset)
+
+data_subset %>%
+  spread(key = observation_type_description, value = species_code) %>% View()
+str(data_subset)
+write.csv('~/Desktop/data_subset.csv')
+
+#subset of voucher files of one species, filter by time 
+PAHE_subset <- currentData %>%
+  filter(species_code == 'PAHE') %>%
+  select(site_id, monitoring_night, call_timestamp, species_code, season)
+print(PAHE_subset)
+class(PAHE_subset)
+write.csv(PAHE_subset, file = '~/Desktop/PAHE_subset.csv')
+write.csv(mutate(PAHE_subset, time=format(call_timestamp, "%Y-%m-%d %H:%M:%S")), file="test.csv", row.names=FALSE)
+
+pahe <- read_csv("~/Desktop/pahe_subset.csv",
+                 locale = locale(tz = "America/Phoenix"))
+pahe
+
+PAHE <- as_tsibble(x = pahe,
+                   time = "call_timestamp",
+                   key = NULL,
+                   index = time,
+                   regular = TRUE) %>%
+  fill_gaps(.full = TRUE) %>%
+  mutate(diff = difference(call_timestamp)) %>%
+  filter(diff > 15 | is.na(diff))
+
+str(pahe)
+duplicates(pahe)
+
 
 # desired output ----------------------------------------------------------------------
 
