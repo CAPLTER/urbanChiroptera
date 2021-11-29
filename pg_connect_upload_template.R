4b12f02bd2be65880088ff4a8f3b5e64ea57bb21
Jessie Dwyer <jdwyer4@asu.edu>
2021-11-29 15:15:27 -0700

    combined file
    deleted one file and renamed another, the same information was in both files, so we only need one

--- pg_connect_upload_template.R
+++ pg_connect_upload_template.R
@@ -1,6 +1,23 @@
-# host: postgres database on localhost
+source('pg_connect_upload_template.R')
+
+# host: login to postgres database on localhost ---------------------------
 pg_local <- dbConnect(dbDriver("PostgreSQL"),
                      user="jdwyer4",
                      dbname="jdwyer4",
                      host="localhost",
                      password=.rs.askForPassword("Enter password:"))
+
+pg <- pg_local
+
+# libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(RPostgreSQL)
+
+# set wd to localrepos "Output Files" folder ------------------------------
+setwd()
+
+# enter filename for output files -----------------------------------------
+
+sonobat_upload(sonobatFile = "5-10_S4Fall_Output.csv")
+
